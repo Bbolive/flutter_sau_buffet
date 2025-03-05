1908d0f9cad3bcf60c0ba3cdf26263b1a054c221
Bbolive <rapatsara.bam@gmail.com>
2025-03-05 12:11:56 +0700

    two commit

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,92 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sau_buffet/views/about_ui.dart';
+import 'package:flutter_sau_buffet/views/cal_bill_ui.dart';
+import 'package:flutter_sau_buffet/views/menu_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeUi extends StatefulWidget {
+  const HomeUi({super.key});
+
+  @override
+  State<HomeUi> createState() => _HomeUiState();
+}
+
+class _HomeUiState extends State<HomeUi> {
+  // ตัวควบคุมการเปลี่ยนหน้า item ของ aimated notch bottom bar
+  final itemCtrl = NotchBottomBarController(index: 1);
+
+  //สรร้างตัวแปรเก็บ Ui ที่จะแสดง ซึ่งล้อกับ item ของ animated notch bottom bar ที่เลือก
+  List<Widget> itemUIShow = [
+    CalBillUi(),
+    MenuUI(),
+    AboutUi(),
+  ];
+
+  //สร้างตัวแปรเก็บ index ของ item ที่เลือก
+  int selectIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text(
+          'Tech SAU BUFFET',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: AnimatedNotchBottomBar(
+        onTap: (valueParam) {
+          setState(() {
+            selectIndex = valueParam;
+          });
+        },
+        notchBottomBarController: itemCtrl,
+        color: Colors.blueAccent,
+        notchColor: Colors.blueAccent,
+        kIconSize: 24.0,
+        kBottomRadius: 28.0,
+        bottomBarItems: [
+          BottomBarItem(
+            itemLabel: 'คิดเงิน',
+            activeItem: Icon(
+              FontAwesomeIcons.moneyBill1Wave,
+              color: Colors.white,
+            ),
+            inActiveItem: Icon(
+              FontAwesomeIcons.moneyBill1Wave,
+              color: Colors.white,
+            ),
+          ),
+          BottomBarItem(
+            itemLabel: 'หน้าหลัก',
+            activeItem: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            inActiveItem: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
+          BottomBarItem(
+            itemLabel: 'เกี่ยวกับ',
+            activeItem: Icon(
+              FontAwesomeIcons.star,
+              color: Colors.white,
+            ),
+            inActiveItem: Icon(
+              FontAwesomeIcons.star,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: itemUIShow[selectIndex],
+    );
+  }
+}
---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sau_buffet/views/home_ui.dart';
+
+class SplashScreenUi extends StatefulWidget {
+  const SplashScreenUi({super.key});
+
+  @override
+  State<SplashScreenUi> createState() => _SplashScreenUiState();
+}
+
+class _SplashScreenUiState extends State<SplashScreenUi> {
+  @override
+  void initState() {
+    // Code splash screen
+    Future.delayed(
+      // ระยะเวลาที่หน่วง
+      Duration(seconds: 3),
+      // พอครบเวลาหน่วงแล้วให้ทำอย่างไร
+      () => Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeUi(),
+        ),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.70,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              'Tech SAU BUFFET',
+              style: TextStyle(
+                fontSize: 28.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Copyright © 2025 by Rapatsara',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
